56bb2aae7d5c273a9884d2f9c7ebc1f08e992d92
Fan4_Metal <fan4_metal@mail.ru>
2022-07-25 09:59:22 +0300

    Add Hidpi support

--- kinolist.gui.py
+++ kinolist.gui.py
@@ -2,15 +2,17 @@ import os
 import winreg
 import threading
 import time
+import ctypes
 
 import wx
 import wx.adv
 import kinolist_lib as kl
 import config 
 
+ctypes.windll.shcore.SetProcessDpiAwareness(2)
 api = config.KINOPOISK_API_TOKEN
 
-VER = "0.3.2"
+VER = "0.3.3"
 REG_PATH = R"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\Winword.exe"
 
 
@@ -32,25 +34,29 @@ def get_reg(name, reg_path):
 
 class InfoPanel(wx.Dialog): 
     def __init__(self, parent, title, id): 
-        super().__init__(parent, title = title, size = (650, 400))
-        
+        super().__init__(parent, title = title)
         filminfo = kl.get_film_info(id, api)
             
         poster = filminfo[9]
-        poster.thumbnail((200, 300))
+        poster.thumbnail(self.FromDIP((200, 300)))
         
         self.panel = wx.Panel(self) 
         self.box1v = wx.BoxSizer(wx.VERTICAL)
         self.box2h = wx.BoxSizer(wx.HORIZONTAL)
-        self.btn = wx.Button(self.panel, wx.ID_OK, label = "OK", size = (100, 25))
+        self.btn = wx.Button(self.panel, wx.ID_OK, label = "OK", size = self.FromDIP((100, 25)))
         self.box1v.Add(self.box2h, proportion = 1, flag = wx.EXPAND | wx.TOP | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5 )
         self.box1v.Add(self.btn, flag = wx.ALIGN_CENTER | wx.TOP | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5 )
         
-        self.image = wx.StaticBitmap(self.panel, wx.ID_ANY, wx.Bitmap(PIL2wx(poster)), size=(200, 300))
+        self.image = wx.StaticBitmap(self.panel, wx.ID_ANY, wx.Bitmap(PIL2wx(poster)), size=self.FromDIP((200, 300)))
         self.box3v = wx.BoxSizer(wx.VERTICAL)
         self.box2h.Add(self.image, flag = wx.TOP | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
         self.box2h.Add(self.box3v, flag = wx.EXPAND | wx.TOP | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
-        self.l_search1 = wx.StaticText(self.panel)
+        self.l_search1 = wx.StaticText(self.panel, style = 0)
+        
+        font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
+        font.SetPointSize(12)
+        self.l_search1.SetFont(font)
+        
         if filminfo[2]:
             self.l_search1.Label = f"{filminfo[0]} ({filminfo[1]}) - Кинопоиск {filminfo[2]}"
         else:
@@ -60,7 +66,8 @@ class InfoPanel(wx.Dialog):
         else:
             cast = f"{filminfo[1]}\n{', '.join(filminfo[3])}\nРежиссер: {filminfo[7][0]}\nВглавных ролях: {', '.join(filminfo[8])}"
         self.l_search2 = wx.StaticText(self.panel, label = cast)
-        self.l_search3 = wx.TextCtrl(self.panel, value = filminfo[4] if filminfo[4] else "Нет описания", style = wx.TE_READONLY | wx.ALIGN_TOP | wx.TE_MULTILINE)
+        self.l_search3 = wx.TextCtrl(self.panel, value = filminfo[4] if filminfo[4] else "Нет описания",
+                                     style = wx.TE_READONLY | wx.ALIGN_TOP | wx.TE_MULTILINE)
         self.box3v.Add(self.l_search1, flag = wx.EXPAND | wx.TOP | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
         self.box3v.Add(self.l_search2, proportion = 1, flag = wx.EXPAND | wx.TOP | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
         self.box3v.Add(self.l_search3, proportion = 1, flag = wx.EXPAND | wx.TOP | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
@@ -114,11 +121,11 @@ class MyFrame(wx.Frame):
         self.Bind(wx.EVT_TEXT_ENTER, self.onEnter)
         # self.t_search.Value = "Матрица"
         
-        self.b_search = wx.Button(self.panel, wx.ID_ANY, size=(100, 25), label='Поиск')
+        self.b_search = wx.Button(self.panel, wx.ID_ANY, size=self.FromDIP((100, 25)), label='Поиск')
         self.gr.Add(self.b_search, pos=(0, 2), flag = wx.TOP | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
         self.Bind(wx.EVT_BUTTON, self.onSearch, id=self.b_search.GetId())
         
-        self.search_list = wx.ListBox(self.panel, wx.ID_ANY, size=(200, 70), style = wx.LB_SINGLE)
+        self.search_list = wx.ListBox(self.panel, wx.ID_ANY, size=self.FromDIP((200, 70)), style = wx.LB_SINGLE)
         self.gr.Add(self.search_list, pos=(1, 1), flag = wx.EXPAND | wx.BOTTOM | wx.LEFT, border = 5)  
         self.Bind(wx.EVT_LISTBOX_DCLICK, self.onInfo, id=self.search_list.GetId())
         self.search_list.Bind(wx.EVT_KEY_DOWN, self.onListEnter)
@@ -129,28 +136,28 @@ class MyFrame(wx.Frame):
         self.Bind(wx.EVT_LISTBOX, self.ListClick2, id=self.film_list.GetId())
         self.Bind(wx.EVT_LISTBOX_DCLICK, self.onInfo, id=self.film_list.GetId())
         
-        self.b_add = wx.Button(self.panel, wx.ID_ANY, size=(100, 25), label='Добавить')
+        self.b_add = wx.Button(self.panel, wx.ID_ANY, size=self.FromDIP((100, 25)), label='Добавить')
         self.gr.Add(self.b_add, pos=(1, 2), flag = wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
         self.Bind(wx.EVT_BUTTON, self.onAdd, id=self.b_add.GetId())
         
-        self.b_info = wx.Button(self.panel, wx.ID_ANY, size=(100, 25), label='Информация')
+        self.b_info = wx.Button(self.panel, wx.ID_ANY, size=self.FromDIP((100, 25)), label='Информация')
         self.gr.Add(self.b_info, pos=(2, 2), flag = wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
         self.Bind(wx.EVT_BUTTON, self.onInfo, id=self.b_info.GetId())
         
-        self.b_delete = wx.Button(self.panel, wx.ID_ANY, size=(100, 25), label='Удалить')
+        self.b_delete = wx.Button(self.panel, wx.ID_ANY, size=self.FromDIP((100, 25)), label='Удалить')
         self.gr.Add(self.b_delete, pos=(3, 2), flag = wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
         self.Bind(wx.EVT_BUTTON, self.onDelete, id=self.b_delete.GetId())
         
-        self.b_up = wx.Button(self.panel, wx.ID_ANY, size=(100, 25), label='\u21e7')
+        self.b_up = wx.Button(self.panel, wx.ID_ANY, size=self.FromDIP((100, 25)), label='\u21e7')
         self.gr.Add(self.b_up, pos=(4, 2), flag = wx.ALIGN_CENTER | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
         self.Bind(wx.EVT_BUTTON, self.onUp, id=self.b_up.GetId())
         
-        self.b_down = wx.Button(self.panel, wx.ID_ANY, size=(100, 25), label='\u21e9')
+        self.b_down = wx.Button(self.panel, wx.ID_ANY, size=self.FromDIP((100, 25)), label='\u21e9')
         self.gr.Add(self.b_down, pos=(5, 2), flag = wx.ALIGN_CENTER_HORIZONTAL | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
         self.Bind(wx.EVT_BUTTON, self.onDown, id=self.b_down.GetId())
         
         
-        self.b_clear = wx.Button(self.panel, wx.ID_ANY, size=(100, 25), label='Очистить все')
+        self.b_clear = wx.Button(self.panel, wx.ID_ANY, size=self.FromDIP((100, 25)), label='Очистить все')
         self.gr.Add(self.b_clear, pos=(6, 2), flag = wx.ALIGN_CENTER_HORIZONTAL | wx.ALIGN_BOTTOM | wx.BOTTOM | wx.LEFT | wx.RIGHT, border = 5)
         self.Bind(wx.EVT_BUTTON, self.onClear, id=self.b_clear.GetId())
         
@@ -162,21 +169,22 @@ class MyFrame(wx.Frame):
         self.gr.AddGrowableCol(1)
         self.gr.AddGrowableRow(6)
         self.panel.SetSizer(self.gr)
-        self.Centre()
         
         
         # ========= Статусбар ===========
         self.statusbar = self.CreateStatusBar(2, style = (wx.BORDER_NONE) & ~(wx.STB_SHOW_TIPS))
-        self.statusbar.SetStatusWidths([80, -1])
+        self.statusbar.SetStatusWidths([self.FromDIP(80), -1])
         self.statusbar.SetStatusText("Фильмов: " + str(len(self.film_id_list)))
+        
         
         # Перехват события наведения на пункт меню, чтобы отключить автоматический показ подсказок в статусбаре
         # wx.EVT_MENU_HIGHLIGHT_ALL(self, self.statusbar_status)
         wx.EvtHandler.Bind(self, wx.EVT_MENU_HIGHLIGHT_ALL, self.statusbar_status)
                 
         self.gauge = wx.Gauge(self.statusbar, range = 100,
-                                pos = (91, 2), size = (self.statusbar.GetSize()[0] - 92, 20),
+                                pos = (self.FromDIP(91), self.FromDIP(2)), size = self.FromDIP((self.statusbar.GetSize()[0] - 72, 20)),
                                 style=wx.GA_HORIZONTAL|wx.GA_PROGRESS|wx.GA_SMOOTH)
+        
         
         
     def statusbar_status(self, event):
@@ -264,9 +272,17 @@ class MyFrame(wx.Frame):
         sel_film_list = self.film_list.GetSelection()
         sel_search_list = self.search_list.GetSelection()
         if sel_film_list != -1:
-            self.info = InfoPanel(self, 'Информация о фильме', self.film_id_list[sel_film_list]).ShowModal()
+            self.info = InfoPanel(self, 'Информация о фильме', self.film_id_list[sel_film_list])
+            self.info.SetClientSize(self.info.FromDIP(wx.Size((750, 400))))
+            self.info.CentreOnParent()
+            self.info.ShowModal()
+            
+            
         elif sel_search_list != -1:
-            self.info = InfoPanel(self, 'Информация о фильме', self.films[sel_search_list][0]).ShowModal()
+            self.info = InfoPanel(self, 'Информация о фильме', self.films[sel_search_list][0])
+            self.info.SetClientSize(self.info.FromDIP(wx.Size((750, 400))))
+            self.info.CentreOnParent()
+            self.info.ShowModal()
     
     def onUp(self, event):
         sel = self.film_list.GetSelection()
@@ -379,6 +395,8 @@ def main():
     app = wx.App()
     top = MyFrame(None, title = "Kinolist GUI")
     top.SetIcon(wx.Icon(kl.get_resource_path("favicon.ico")))
+    top.SetClientSize(top.FromDIP(wx.Size(500, 500)))
+    top.Centre()
     top.Show()
     app.MainLoop()
 
